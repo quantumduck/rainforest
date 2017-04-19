838ebc996aaae5a440455acf9dfefcf0e38ae318
quantumduck <quantum.duck@hotmail.com>
2017-04-19 13:56:07 -0400

    making changes to index view with useful methods in product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,4 +7,14 @@ class Product < ApplicationRecord
     '$' + dollars + padding + cents
   end
 
+  def short_description(max_length)
+    if description.length < max_length
+      description.split("\n").first
+    elsif description.split("\n").first.length <= (max_length - 3)
+      description.split("\n").first + "..."
+    else
+      description.split("\n").first[0, max_length - 3] + "..."
+    end
+  end
+
 end
