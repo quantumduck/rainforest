1ffa9376b176cbe5f5602e8e9ef88bb8c3c08ea0
quantumduck <quantum.duck@hotmail.com>
2017-04-19 16:11:21 -0400

    displaying user name when logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+private
+
+  helper_method :current_user
+
+  def current_user
+    #||= updates truthiness of @current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    #||= will only update user if it does not already exist.
+  end
 end
