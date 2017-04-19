3b656b0e8f354eba67d31b8fccb103045ec367bd
quantumduck <quantum.duck@hotmail.com>
2017-04-19 13:33:39 -0400

    basic views filled in

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,10 @@
 class Product < ApplicationRecord
+
+  def price
+    dollars = (price_in_cents / 100).to_s
+    padding = ((price_in_cents % 100) < 10)? ".0": "."
+    cents = (price_in_cents % 100).to_s
+    '$' + dollars + padding + cents
+  end
+
 end
