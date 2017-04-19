8fc77a0fed303b53df2c755e545ef9342cf6ed75
quantumduck <quantum.duck@hotmail.com>
2017-04-19 14:17:13 -0400

    adding user model, controller, routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+
   root 'products#index'
 
   resources :products #only: %i(index new show edit)
+  resource :users, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
