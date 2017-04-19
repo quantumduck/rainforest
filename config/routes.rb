5b297d66cdde9fb641a4d4ccaa307aac755df8f8
quantumduck <quantum.duck@hotmail.com>
2017-04-19 15:03:24 -0400

    sessions controller methods

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
 
   resources :products #only: %i(index new show edit)
   resource :users, only: [:new, :create]
+  resource :sessions, only: %i(new create destroy)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
