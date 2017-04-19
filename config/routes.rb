e3c67b195fc53412e5f7ac88659161e11c6cf5ed
quantumduck <quantum.duck@hotmail.com>
2017-04-19 15:31:13 -0400

    customized urls for routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   root 'products#index'
 
   resources :products #only: %i(index new show edit)
-  resource :users, only: [:new, :create]
-  resource :sessions, only: %i(new create destroy)
+  # resource :users, only: [:new, :create]
+  get "/sign_up", to: 'users#new'
+  post "/sign_up", to: 'users#create', as: :users
+  # resource :sessions, only: %i(new create destroy)
+  get "/login", to: 'sessions#new'
+  post "/login", to: 'sessions#create'
+  delete "/login", to: 'sessions#delete'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
